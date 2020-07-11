fe6706f982aa657aeb83ab504d02ead62bd42f7f
awis-mbp <awis@awislabs.com>
2020-07-11 10:06:48 +0800

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Awis Alkarni on 11/07/2020.
+//  Copyright © 2020 Awislabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
